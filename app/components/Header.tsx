d38b62469218ebbfa1a22c35f66a57b45ce51963
endoritos <endysempai@gmail.com>
2024-05-21 11:54:01 +0200

    Navbar and footer

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createClient } from "@/prismicio";
 import { link } from "fs";
 import { PrismicNextLink } from "@prismicio/next";
+import NavBar from "@/app/components/NavBar";
 
 
 
@@ -12,20 +13,7 @@ export default async function Header() {
 
     return (
         <header className="top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4">
-            <nav>
-                <ul>
-                    <li>
-                        <a href="/" aria-label="Home Page">
-                            {settings.data.name}
-                        </a>
-                    </li>
-                    {settings.data.nav_item.map(({link, label}, index) => (
-                        <li key={index}>
-                            <PrismicNextLink field={link}>{label}</PrismicNextLink>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
+            <NavBar settings={settings}/>
         </header>
     )
 }
