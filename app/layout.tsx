dd335e63939e3e75dc7ffd6d7f469d30699ef9e0
endoritos <endysempai@gmail.com>
2024-05-22 23:27:39 +0200

    trakkker

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,11 +33,11 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-slate-900">
       <body className={clsx(urbanist.className,"relative min-h-sreen")}>
-        <Header/>
+        <Header/><Analytics/>
         {children}
         <div className="absolute inset-0 -z-50 max-h-screen background-gradient"></div>
         <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
-        <Footer/><Analytics/>
+        <Footer/>
       </body>
       <PrismicPreview repositoryName={repositoryName}/>
     </html>
