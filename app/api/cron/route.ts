d537382734f5be699bde1a2796601e45f767f7a1
endoritos <endysempai@gmail.com>
2024-06-11 22:03:57 +0200

    up caching

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -2,17 +2,20 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
   const authorization = req.headers.get('authorization');
-  
+
   if (authorization !== `Bearer ${process.env.CLIENT_KEY}`) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
 
   try {
-    // Trigger revalidation
-    const revalidateUrl = new URL('*', req.url);
+    const revalidateUrl = new URL('/api/revalidate', req.url);
     await fetch(revalidateUrl.toString(), { method: 'POST' });
     return NextResponse.json({ message: 'Cache cleared' });
   } catch (error) {
     return NextResponse.json({ error: 'Failed to revalidate' }, { status: 500 });
   }
 }
+
+export const config = {
+  runtime: 'edge',
+};
